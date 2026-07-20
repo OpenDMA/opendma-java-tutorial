86627e23efa88d1f712be41a33eadd2013621049
Stefan Kopf <s.kopf@xaldon.de>
2026-07-20 13:01:10 +0200

    Update to xmlrepo 0.8.1 fix and add tests

---
+++ src/test/java/org/opendma/tutorial/AllLessionsTests.java
@@ -0,0 +1,87 @@
+package org.opendma.tutorial;
+
+import org.junit.Test;
+
+public class AllLessionsTests {
+    
+    @Test
+    public void testLesson1() throws Exception {
+        Lession01_ObtainASession.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson2() throws Exception {
+        Lession02_ListVisibleRepositories.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson3() throws Exception {
+        Lession03_GetRepository.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson4() throws Exception {
+        Lession04_RuntimeTypeInformation.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson5() throws Exception {
+        Lession05_ClassDependentInterfaces.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson6() throws Exception {
+        Lession06_SimplePrintObjectProperties.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson7() throws Exception {
+        Lession07_PrintObjectProperties.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson8() throws Exception {
+        Lession08_InheritancePath.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson9() throws Exception {
+        Lession09_PrintClassTree.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson10() throws Exception {
+        Lession10_PrintFolderTree.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson11() throws Exception {
+        Lession11_PrintFolderContent.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson12() throws Exception {
+        Lession12_PrintSampleDocumentProperties.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson13() throws Exception {
+        Lession13_PrintDocumentContentElements.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson14() throws Exception {
+        Lession14_RetrieveDataFromContentElements.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson15() throws Exception {
+        Lession15_PrintDocumentVersions.main(new String[0]);
+    }
+    
+    @Test
+    public void testLesson16() throws Exception {
+        Lession16_PrintFolderAssociations.main(new String[0]);
+    }
+
+}
